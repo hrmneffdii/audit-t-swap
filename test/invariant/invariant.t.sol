ae214445cac10b67bf3e9eb3ad0e5708c9b33b49
Herman Effendi <hermaneffendi0502@gmail.com>
2024-08-05 19:18:56 +0700

    config invariant

--- test/invariant/invariant.t.sol
+++ test/invariant/invariant.t.sol
@@ -1,27 +1,24 @@
 // SPDX-License-Identifier: SEE LICENSE IN LICENSE
 pragma solidity 0.8.20;
 
-import {Test} from "@forge-std/Test.sol";
-import {StdInvariant} from "@forge-std/StdInvariant.sol";
+import {Test, StdInvariant} from "@forge-std/Test.sol";
 import {ERC20Mock} from "../mocks/ERC20Mocks.sol";
 import {PoolFactory} from "../../src/PoolFactory.sol";
 import {TSwapPool} from "../../src/TSwapPool.sol";
 import {Handler} from "../invariant/handler.t.sol";
 
 contract Invariant is StdInvariant, Test{
-    // we have two assets
+    PoolFactory factory;
+    TSwapPool pool;
     ERC20Mock poolToken;
     ERC20Mock weth;
 
-    Handler handler;
-
-    // we are gonna need the contract
-    PoolFactory factory;
-    TSwapPool pool;
-
-    // starting amount
-    int256 constant STARTING_X = 100e18; // Starting ERC20 / X
+    int256 constant STARTING_X = 100e18; // Starting ERC20 
     int256 constant STARTING_Y = 50e18;  // Starting WETH
+    uint256 constant FEE = 997e15;
+    int256 constant MATH_PRECISION = 1e18;
+
+    Handler handler;
 
     function setUp() public {
         weth = new ERC20Mock();
@@ -29,13 +26,13 @@ contract Invariant is StdInvariant, Test{
         factory = new PoolFactory(address(weth));
         pool = TSwapPool(factory.createPool(address(poolToken)));
 
-        // create those balances x and y
+        // create the initial values for the pool 
         poolToken.mint(address(this), uint256(STARTING_X));
         weth.mint(address(this), uint256(STARTING_Y));
 
         // approve
-        poolToken.approve(address(pool), uint256(STARTING_X));
-        weth.approve(address(pool), uint256(STARTING_Y));
+        poolToken.approve(address(pool), type(uint256).max);
+        weth.approve(address(pool), type(uint256).max);
 
         // deposit
         pool.deposit(
@@ -46,12 +43,20 @@ contract Invariant is StdInvariant, Test{
         );
 
         handler = new Handler(pool);
+
         bytes4[] memory selectors = new bytes4[](2);
         selectors[0] = handler.deposit.selector;
         selectors[1] = handler.swapPoolTokenForWethBasedOnOutputWeth.selector;
+
+        targetSelector(FuzzSelector({addr: address(handler), selectors: selectors}));
+        targetContract(address(handler));
     }
 
     function invariant_constantFormulaProductStaysTheSame() public view {
         assertEq(handler.actualDeltaX(), handler.expectedDeltaX());
+    }
+
+    function invariant_deltaYFollowsMath() public view {
+        assertEq(handler.actualDeltaY(), handler.expectedDeltaY());
     }
 }
